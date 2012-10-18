b1e84ab1a6cb2e686f3d6fc350e134a4d086f6ce
Rob Mayoff <git@rob.dqd.com>
2012-10-17 22:17:02 -0500

    Initial Commit

---
+++ splitview/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  splitview
+//
+//  Created by Rob Mayoff on 10/17/12.
+//  Copyright (c) 2012 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ splitview/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  splitview
+//
+//  Created by Rob Mayoff on 10/17/12.
+//  Copyright (c) 2012 Rob Mayoff. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
